8d0c402ed497fee948ceac569b32bbdd3660eef7
saber <sbaerm.com@gmail.com>
2023-07-04 15:34:26 +0330

    add main for deadlock

---
+++ src/main/java/deadlock/Main.java
@@ -0,0 +1,17 @@
+package deadlock;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        final String resource1 = "hello from resource one";
+        final String resource2 = "hello from resource two";
+
+        Thread t1 = new ThreadOne(resource1, resource2);
+        Thread t2 = new ThreadTwo(resource1, resource2);
+
+        t1.start();
+        t2.start();
+    }
+}
+
